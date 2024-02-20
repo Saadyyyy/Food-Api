ef54e849ed7ebeb93b7f1ba4addcc959e767680e
Saadyyyy <laodesaady12345@gmail.com>
2024-02-20 12:34:00 +0700

    add route update and get by id

--- router/router.go
+++ router/router.go
@@ -16,8 +16,10 @@ func Api(r *gin.Engine, db *sql.DB) {
 
 	food := r.Group("food")
 	{
-		food.POST("/", handl.Create)
-		food.GET("/", handl.GetAll)
-		food.DELETE("/:id", handl.Delete)
+		food.POST("/", handl.Create)      //fungsi create
+		food.GET("/", handl.GetAll)       // fungso get all
+		food.DELETE("/:id", handl.Delete) //fungsi delete
+		food.GET("/:id", handl.GetById)   //fungsi get by id
+		food.PUT("/:id", handl.Update)    //fungsi update
 	}
 }
